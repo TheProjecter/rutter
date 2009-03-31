91b3a4d6885f34d685d8d1329053e060f9fc03dd
thetrancedj <thetrancedj@06e063be-1a1b-11de-be07-9135ae5da72a>
2009-03-31 16:15:23 +0000

    Начальный импорт. Установленный CakePHP. Интегрированы средства дебага от FCFramework. Класс Вида заменен на Smarty.

---
+++ app/includes.php
@@ -0,0 +1,4 @@
+<?php 
+require_once 'includes/select.php';
+require_once 'includes/debug.php';
+?>
---
+++ app/templates_c/%%6D^6D7^6D7C5625%%test.tpl.php
@@ -0,0 +1,4 @@
+<?php /* Smarty version 2.6.22, created on 2009-03-31 16:51:20
+         compiled from test.tpl */ ?>
+<h1>Hello, <?php echo $this->_tpl_vars['name']; ?>
+!</h1>
